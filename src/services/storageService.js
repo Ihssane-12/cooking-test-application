6bde733c34ab4a1f626b3bd0ad3d76b10e34815b
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-02-28 00:45:21 +0000

    finished the storagefile

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -1,11 +1,15 @@
 export const saveFavorite = (recipe) => {
     let favs = JSON.parse(localStorage.getItem('favs')) || [];
-    if (!favs.some(r => r.id === recipe.id)) {
+    if (!favs.find(r => r.id === recipe.id)) {
+        favs.push(recipe);
         localStorage.setItem('favs', JSON.stringify(favs));
+    }
 };
-}
-export const getFavorites = () => JSON.parse(localStorage.getItem('favs')) || [];
-export const removeFavorite = (id) => {
-    favs = favs.filter(r => r.id !== id);
 
-}
+export const getFavorites = () => JSON.parse(localStorage.getItem('favs')) || [];
+
+export const removeFavorite = (id) => {
+    let favs = getFavorites();
+    favs = favs.filter(r => r.id !== id);
+    localStorage.setItem('favs', JSON.stringify(favs));
+};
