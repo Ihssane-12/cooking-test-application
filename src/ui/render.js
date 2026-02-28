7573ace1f01596f8bfa66d31900750ed4b868e9f
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-02-28 00:48:33 +0000

    add favorite button to recipe cards

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,3 +1,4 @@
 const card = document.createElement('div');
 card.className = 'recipe-card';
 const badge = getNutriBadge(recipe.caloriesPerServing);
+<i class="fas fa-heart fav-btn ${isFavPage ? 'active' : ''}" data-id="${recipe.id}"></i>
