abe770dcd367310f7ef12610a3012a8cc8402ba2
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-02-28 00:41:04 +0000

    add getFavorites function

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -2,4 +2,6 @@ export const saveFavorite = (recipe) => {
     let favs = JSON.parse(localStorage.getItem('favs')) || [];
     if (!favs.some(r => r.id === recipe.id)) {
         localStorage.setItem('favs', JSON.stringify(favs));
-};}
+};
+}
+export const getFavorites = () => JSON.parse(localStorage.getItem('favs')) || [];
