694e9e51dcfea95e0aa0707e0e7a64e0451f9dea
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-02-28 00:55:54 +0000

    add home page recipes display

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import { renderRecipes } from './ui/render.js';
 import { saveFavorite, getFavorites } from './services/storageService.js';
 
 const mainContent = document.getElementById('main-content');
+const loadHome = async () => {
+    mainContent.innerHTML = '<h2>History</h2><div id="recipe-list"></div>';
+    const recipes = await getAllRecipes();
+    renderRecipes(document.getElementById('recipe-list'), recipes);
+};
