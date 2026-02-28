b786dc6c9ab596b2f36c77400695a510ccf9e303
ihsane ben-mouina <nom.prenom@univ-lille1.fr>
2026-02-28 00:51:39 +0000

    add ingredients section

--- src/ui/render.js
+++ src/ui/render.js
@@ -6,5 +6,13 @@ card.addEventListener('click', (e) => {
     if(!e.target.classList.contains('fav-btn')) showModal(recipe);
 });
 export const showModal = (recipe) => {
-    
-}
+    dynamicContent = `
+<div class="details-grid">
+    <div class="recipe-details">
+        <h2>${recipe.title}</h2>
+        <p>${recipe.description}</p>
+        <div class="badge" style="background-color: ${badge.color};">${badge.label}</div>
+    </div>
+</div>`;
+    document.getElementById('dynamic-content').innerHTML = dynamicContent;
+};
